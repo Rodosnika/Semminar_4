322bb1f9e78b7411e7fde54629e0042a9ab11237
Veronika <vvnika@mail.ru>
2022-07-12 21:06:07 +1100

    add " Домашняя работа Семинар_4 Задача_27"

--- Program.cs
+++ Program.cs
@@ -2,13 +2,14 @@
 // на вход два натуральных числа (A и B) и возводит число A в степень B.
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16 
-
+/*
 Console.WriteLine($"\nЗадача 25. Возведене числа A в натуральную степень B");
 
 int Exponentiation(int numberA, int numberB)
 {
     int result = 1;
     for (int i = 1; i <= numberB; i++)
+
     {
         result = result * numberA;
     }
@@ -23,6 +24,31 @@ int numberB = Convert.ToInt32(Console.ReadLine());
 
 int exponentiation = Exponentiation(numberA, numberB);
 Console.WriteLine("Ответ: " + exponentiation);
+*/
 
+// Задача 27: Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+Console.WriteLine($"\nЗадача 27. Выдаёт сумму цифр в числе");
+Console.Write("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
 
+  int SumNumber(int numberN){
+    
+    int counter = Convert.ToString(numberN).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++){
+      advance = numberN - numberN % 10;
+      result = result + (numberN - advance);
+      numberN = numberN / 10;
+    }
+   return result;
+  }
+
+int sumNumber = SumNumber(numberN);
+Console.WriteLine("Сумма цифр в числе: " + sumNumber);
 
